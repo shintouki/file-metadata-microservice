4e8b2a4649c32e62b4d5e92746bd05c114c7095d
Ted Shin <tdyshin@gmail.com>
2016-07-26 18:30:54 +0000

    Add file metadata microservice

--- server.js
+++ server.js
@@ -1,4 +1,30 @@
 // File Metadata Microservice
 
 var express = require('express');
+var multer = require('multer');
+
+var app = express();
+var upload = multer().single('uploaded-file');
+
+var port = process.env.PORT || 8080;
+
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+  
+});
+
+app.post('/filesize', function(req, res) {
+    upload(req, res, function(err) {
+        if (err) throw err;
+        var fileSize = req.file.size;
+        console.log(fileSize);
+        res.send( {filesize: fileSize} );
+    });
+    
+});
+
+app.listen(port, function () {
+  console.log('App is running on port ' + port);
+});
 
